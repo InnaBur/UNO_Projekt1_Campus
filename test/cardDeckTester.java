1faaa4f26bfb1c1a698a8bea1d90ce7bb995f2da
Inna Burlaka <inna_burlaka@ukr.net>
2025-06-26 16:14:41 +0200

    Bots automatisierung - in progress

--- test/cardDeckTester.java
+++ test/cardDeckTester.java
@@ -1,6 +1,6 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-
 
 import java.util.ArrayList;
 
@@ -9,18 +9,39 @@ public class cardDeckTester {
 
 
     ArrayList<Card> cards = new ArrayList<>();
+    CardsDeck cardsDeck;
+
+    @Before
+    public void setCardsDeck() {
+        cardsDeck = new CardsDeck(cards);
+    }
 
 
     @Test
     public void createDeckTest() {
-        CardsDeck cardsDeck = new CardsDeck(cards);
-
-
         cardsDeck.createCardDeck();
-        String cardName = cardsDeck.getCardsDeck().get(cards.size()-1).getCardName();
+        String cardName = cardsDeck.getCardsDeck().get(cards.size() - 1).getCardName();
         String result = "cc";
 
         // Case-insensitive Vergleich
         Assert.assertEquals(result.toLowerCase(), cardName.toLowerCase());
+    }
+
+    @Test
+    public void cardsTest() {
+
+
+        cardsDeck.createCardDeck();
+        ArrayList<Card> cards = cardsDeck.getCardsDeck();
+        int count = 0;
+
+        for (Card card : cards) {
+            if (card.getCardName().equals("R3")) {
+                count++;
+            }
+        }
+        int result = 2; //2 Karten jede Fahrbe und Anzahl
+
+        Assert.assertEquals(result, count);
     }
 }
